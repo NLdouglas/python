a715c941f9a1aa17de76dd0f8b05d6f4db738885
Douglas <douglasleodoro.qi@gmail.com>
2022-01-20 22:41:35 -0300

    modulos
    exercicios finais de modulos

---
+++ exe19..py
@@ -0,0 +1,5 @@
+import imp
+import random
+alunos = 'arthur', 'douglas', 'cibele', 'gabriela'
+ajuda = random.choice(alunos)
+print('os alunos são {} \n e o escolhido para ajudar sera {}'.format(alunos, ajuda))
