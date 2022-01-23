9ff01adad555a8af1984d546a939d9856b03a937
Douglas <douglasleodoro.qi@gmail.com>
2022-01-23 03:10:16 -0300

    0

---
+++ .vscode/manipulação de texto/mantexto.py
@@ -0,0 +1,16 @@
+# fatiamento  
+
+frase = 'Curso em Video Python'
+#print(frase[3: 13]) # terceiro cacter ao décimo terceiro
+# [:13] do inicio ao decimo3
+# [1:15:2] do N 1 ao 15 pulando de 2 em 2
+# [::2] string inteira pulando de 2 em 2, ''' = 3 aspas para usar em texto longos
+#.upper = joga a string toda em maisculo, lower = string em minusculo
+# len = conta o tamanho completo da fras incluindo espaços
+# .strip() = corta os espaços indesejados 
+# frase.replace('pytho', 'android') = a string so muda efetivamente se for o replace for usado em outra atribuição 
+# print('curso' in frase) = se a palavra curso esta na string = retorna true or false
+#frase.find('curso') = retorna 0 pois curso se inicia na posição inicial
+dividido = frase.split()
+print(dividido[2] [3]) # = moste o segundo item dentro da string completa e me mostre a terceira letra do item pego
+
