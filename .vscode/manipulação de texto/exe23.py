5af5df49b5c0ad70778c2da883c434404b653eb3
Douglas <douglasleodoro.qi@gmail.com>
2022-01-24 01:57:49 -0300

    update exe022.py
    é

---
+++ .vscode/manipulação de texto/exe23.py
@@ -0,0 +1,7 @@
+name = str(input('qual seu nome:' ))
+print('minusculo {}'.format(name.lower()))
+print('maisucula {}'.format(name.upper()))
+print('quantidade {}'.format(len(name.strip())))
+parte = name.split()
+print('primeiro nome {}'.format(parte[0]))
+             
