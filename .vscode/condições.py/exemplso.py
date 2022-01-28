730ed8216211c2ef723fa80c7c1e113fcd0e8852
Douglas <douglasleodoro.qi@gmail.com>
2022-01-27 23:56:00 -0300

    exe36.py
    revisar codigo

--- .vscode/condições.py/exemplso.py
+++ .vscode/condições.py/exemplso.py
@@ -1,1 +1,13 @@
-aa
+casa = float(input('digite o valor da casa: '))
+salario = float(input('seu salario: '))
+anos = int(input('prestações: '))
+valor = casa / (anos * 12) 
+porcentagem = salario * 30 / 100 
+
+print('para pagar uma casa de {:.2f} em {} anos '.format(casa, anos), end='')
+print('a prestação sera de R${:.2f}'.format(valor))
+
+if valor <= porcentagem :
+    print('emprestimo negado')
+else:
+    print('emprestimo aceito')
