1ebb69ccd5e6c5fa30691891bd8f0e78dc5cba47
Douglas <douglasleodoro.qi@gmail.com>
2022-02-03 21:40:09 -0300

    media pronta

--- .vscode/laços.py/exe56.py
+++ .vscode/laços.py/exe56.py
@@ -1,3 +1,12 @@
-# dado = pessoas 
-# fazer laco com pessoas contendo nome, idade e sexo
-#mandar o resulta para dados separadamente 
+somaidade = 0
+for d in range(1, 5):
+    print('{}ª PESSOA'.format(d))
+    nome = input('Nome: ')
+    idade = int(input('Idade: '))
+    sexo = input('Sexo [M/F]: ')
+    somaidade += idade
+mediaidade = somaidade / 4 
+print('a media de idade do grupo é {}'.format(mediaidade))
+#verificar media de idade
+#homem mais velho
+#quantidade de mulheres com menos de 20 anos
