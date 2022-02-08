05e9bc6a99b9b79b4c0d23f215df39ef3ea2f397
Douglas <douglasleodoro.qi@gmail.com>
2022-02-07 23:04:43 -0300

    exe57W.py

---
+++ .vscode/laços.py/exe57W.py
@@ -0,0 +1,6 @@
+sexo = 'M' or 'F'
+while sexo == sexo:
+    sexo = str('qual o seu sexo[M/F]: ').upper()
+    if sexo != sexo:
+        print('este valor não é aceito, somente as letras M ou F')
+print('obrigado pela respostafd')
