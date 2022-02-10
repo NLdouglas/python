19a4b44d72321680b6eb1a026815de71ad21f332
Douglas <douglasleodoro.qi@gmail.com>
2022-02-09 23:16:55 -0300

    exe58W.py

--- .vscode/laços.py/exe58W.py
+++ .vscode/laços.py/exe58W.py
@@ -1,3 +1,10 @@
-#random de 0 a 10 
+from random import randint
 #laço pra ler a saida e entrada do usuario
+palpite = int(input('digite um numero de 0 a 10: '))
+maquina = randint(0, 10)
+cont = 0
+while palpite != maquina:
+    cont += 1
+    palpite = int(input('seu palpite foi {} e da maquina foi {}, tente novamente: '.format(palpite, maquina)))
+    if pal
 #contador dos palpites 
