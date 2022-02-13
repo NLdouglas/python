df4791e5c304e64f47ccb02f2f875b6164dadc4d
Douglas <douglasleodoro.qi@gmail.com>
2022-02-13 00:05:57 -0300

    62

---
+++ .vscode/laços.py/exe61W.py
@@ -0,0 +1,9 @@
+termo = int(input('digite o termo: '))
+razao = int(input('digite a razao: '))
+primeiro = termo
+cont = 1
+while cont <=10:
+    print('{} - '.format(primeiro), end='')
+    primeiro += razao
+    cont +=1
+print('fim')
