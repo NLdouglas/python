6d6030137c49cda8098aadb6e360db0552fd0f5a
Douglas <douglasleodoro.qi@gmail.com>
2022-02-12 23:23:17 -0300

    exe59 resolvido

--- .vscode/laços.py/exe59W.py
+++ .vscode/laços.py/exe59W.py
@@ -2,25 +2,30 @@ valor1 = int(input('digite um valor: '))
 valor2 = int(input('digite outro valor: '))
 soma = valor1 + valor2
 multiplicar = valor1 * valor2
-final = False
-while not final:
+final = True
+while final:
     print('[1]somar\n[2]multiplicar\n[3]maior\n[4]novos numeros\n[5]sair do programa')
     resposta = int(input('escolha um numnero acima: '))
     if resposta == 1:
         print(soma)
-    if resposta == 2:
-         print(multiplicar)
-    if resposta == 3:
+    elif resposta == 2:
+        multiplicar = valor1 * valor2
+        print(multiplicar)
+    elif resposta == 3:
         if valor1 > valor2:
             print(valor1)
         else: 
             print(valor2)
-    if resposta == 4:
+    elif resposta == 4:
         print('escolha os valores novamente!')
         valor1 = int(input('digite um valor: '))
         valor2 = int(input('digite outro valor: '))
-    if resposta == 5:
-        final == True
+        soma = valor1 + valor2
+    elif resposta == 5:
+        final = False
+    else:
+        print('opção invalida tente novamente')
+    print('=-=' *10)
     
 print('voce finalizou o programa')
 
