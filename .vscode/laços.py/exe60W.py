88057e3b12509046b381c02898890fdfbd05e1ac
NLdouglas <douglasleodoro.qi@gmail.com>
2022-02-14 23:43:41 -0300

    refazendo exe60 while

--- .vscode/laços.py/exe60W.py
+++ .vscode/laços.py/exe60W.py
@@ -1,18 +1,16 @@
 # resolução com modulo
-from math import factorial
+'''from math import factorial
 n = int(input('digite um numero: '))
 f = factorial(n)
-print('o fatoria de {} é {}'.format(n, f))
+print('o fatoria de {} é {}'.format(n, f))'''
 
 # forma sem o modulo
 
+
 n = int(input('digite um numero: '))
 c = n
-f = 1 # f = 1 devido a multiplicação sendo todo n mult por 0 = 0 assim n iniciando o laço
-print('calculando {}! ='.format(n), end='')
-while c >0:
-    print('{}'.format(c), end='')
-    print('x ' if c > 1 else ' = ', end='')
-    f *= c
+while c > 0:
     c -= 1
-print('{}'.format(f))
+    print('{}'.format(c), end=' ')
+
+    
