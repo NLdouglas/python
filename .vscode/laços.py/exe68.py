74066dc17c8f7fff7c2a2d13a1a6546cf2f6fe9a
Douglas <87586242+NLdouglas@users.noreply.github.com>
2022-03-14 22:02:32 -0300

    terminar exe69

--- .vscode/laços.py/exe68.py
+++ .vscode/laços.py/exe68.py
@@ -1,26 +1,35 @@
 from random import randint
-
+v = 0
 while True:
-    jogador = int(input('diga um valor: '))
+    jogador = int(input('digite um valor? '))
     computador = randint(0, 10)
-    total = jogador + computador 
-    tipo = ' '
+    total = jogador + computador
+    tipo = ' ' # flag para iniciar outro laço, variavel contem string vazia pois vai receber input com resposta do usuario 
     while tipo not in 'PI':
-        tipo = str(input('Par ou Impar [P/I] ? ')).upper().strip()[0]
-    print(f'voce jogou {jogador} e o computador {computador}. total de {total}')
-    if tipo == 'p':
-        if total % 2 == 0:
-            print('Voce venceu')
-            v += 1
-        else:
-            print('Voce perdeu')
-            break
+        tipo = str(input('Par ou Ímpar? [P/I] ')).upper().strip()[0] #saida do usuario ira se tornar o valor de tipo
+    print(f'Voce jogou {jogador} e o computador {computador}. total {total}', end=' ')
+    print('Deu Par' if total % 2 == 0 else 'Deu Ímpar')
+    if tipo == 'P':
+            if total % 2 == 0:
+                print('voce ganhou')
+                v += 1
+            else:
+                print('voce perdeu')
+                break
     elif tipo == 'I':
-        if total % 2 == 1:
-            print('Voce venceu')
-            v += 1
-        else:
-            print('Voce perdeu')
-            break
-    print('Vamos jogar novamente....')
-print(f'game over! voce venceu {v} vezes')
+            if tipo % 2 == 1:
+                print('voce ganhou')
+                v +=1
+            else:
+                print('voce perdeu')
+                break
+    print('vamos jogar novamente...')
+print(f'Game over! voce venceu {v} vezes')
+
+'''
+Primeiro laço serve para inciar sempre o programa com as pergunda de valor
+ja o segundo serve para iniciar a resposta do jogador referente a se ele quer PAr ou Impar
+dando em seguida a validação dos resultados sendo os IFS para conferir se deu par ou impar
+ficar atento as identações dos prints e condições
+
+ '''
---
+++ .vscode/laços.py/exe69.py
@@ -0,0 +1,16 @@
+from operator import truediv
+
+
+continuar = ' '
+homem = 0
+mulher = 0 
+while True:
+    print('-'*20)
+    print('CADASTRE UMA PESSOA')
+    print('-'*20)
+    idade = int(input('idade: '))
+    sexo = str(input('Sexo: [M/F]'))
+    while continuar not in 'N':
+
+     
+    
