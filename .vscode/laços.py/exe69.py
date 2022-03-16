e938ea90ea438488bbc517b2e35595182a79e1cd
Douglas <87586242+NLdouglas@users.noreply.github.com>
2022-03-15 22:18:43 -0300

    exe69

--- .vscode/laços.py/exe69.py
+++ .vscode/laços.py/exe69.py
@@ -1,7 +1,4 @@
-from operator import truediv
-
-
-continuar = ' '
+pessoas = 0
 homem = 0
 mulher = 0 
 while True:
@@ -9,8 +6,41 @@ while True:
     print('CADASTRE UMA PESSOA')
     print('-'*20)
     idade = int(input('idade: '))
-    sexo = str(input('Sexo: [M/F]'))
-    while continuar not in 'N':
+    sexo = str(input('Sexo: [M/F] ')).upper()
+    if idade > 18:
+        pessoas += 1
+    if sexo == 'M':
+        homem += 1
+    elif sexo == 'F' and idade < 20:
+        mulher +=1
+    print('-'*20)
+    continuar = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+    if continuar == 'N':
+        break
+print(f'total de pessoas com mais de 18 anos: {pessoas}')
+print(f'Ao todo temos {homem} homem cadastrado')
+print(f'E temos {mulher} mulheres com menos de 20 anos')
 
-     
-    
+ # RESPOSTA CURSO EM VIDEO
+
+
+homem = mulher = pessoas = 0
+while True:
+    idade = int(input('idade: '))
+    sexo = ' ' 
+    while sexo not in 'MF':
+        sexo = str(input('sexo: [M/F]')).strip().upper()[0]
+    if idade >= 18:
+        pessoas += 1
+    if sexo == 'M':
+        homem += 1
+    if sexo == 'F' and idade < 20:
+        mulher += 1
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break    
+print(f'total de pessoas com mais de 18 anos: {pessoas}')
+print(f'Ao todo temos {homem} homem cadastrado')
+print(f'E temos {mulher} mulheres com menos de 20 anos')
