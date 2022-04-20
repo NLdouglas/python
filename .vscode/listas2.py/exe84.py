6643c8ce9db2a5d8c7ccf201447dc3eb4d2dd609
Douglas <87586242+NLdouglas@users.noreply.github.com>
2022-04-19 22:17:37 -0300

    exe86

--- .vscode/listas2.py/exe84.py
+++ .vscode/listas2.py/exe84.py
@@ -1,24 +1,29 @@
-pessoas = []
-dados_pessoas = []
-pesomax = pesomin = 0
+temp = []
+princ = []
+mai = men = 0
 while True:
-    dados_pessoas.append(str(input('Nome: ')))
-    dados_pessoas.append(int(input('Peso: ')))
-    pessoas.append(dados_pessoas)
-
-    for peso in pessoas:
-        if peso[1] == max(peso):
-            pesomax = max(peso)
-            
-
-
-
-
-    
-    resp = ' '
-    while resp not in 'NS':
-        resp = str(input('quer continuar? [S/N] ')).upper().strip()
-    if resp == 'N':
+    temp.append(str(input('nome: ')))
+    temp.append(float(input('peso: ')))
+    if len(princ) == 0:
+        mai = men = temp[1] # 1 == PESO 0 == nome
+    else:
+        if temp[1] > mai:
+            mai = temp[1]
+        if temp[1] < men:
+            men = temp[1]
+    princ.append(temp[:]) # princ faz uma copia de temp sendo duas listas diferente mas com os mesmos valores
+    temp.clear() #limpa a lista temp
+    resp = str(input('quer continuar? [S/N]'))
+    if resp in 'Nn':
         break
-print(len(pessoas)) # QUANTIDADE DE PESSOAS CADASTRADASw
-print(pesomax)
+print(f'tamanho da lista {len(princ)}')
+print(f'o maior peso foi de {mai}kg', end=' ')
+for p in princ:
+    if p[1] == mai:
+        print(f'{p[0]}', end=' ')
+print()
+print(f'o menor peso foi de {men}kg', end=' ')
+for p in princ:
+    if p[1] == men:
+        print(f'{p[0]}', end=' ')
+print()
