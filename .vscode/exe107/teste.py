bed796495fcb280f0b84898f1fcc37ae25cb614c
Douglas <87586242+NLdouglas@users.noreply.github.com>
2022-05-11 21:17:08 -0300

    fim de modulos e exercicios

---
+++ .vscode/exe107/teste.py
@@ -0,0 +1,6 @@
+import moeda
+#from moeda import metade, dobro, aumentar, moeda
+#from exe107 import moeda
+
+p = float(input('digite o preço: '))
+moeda.resumo(p)
--- .vscode/uteis/aula22/aula.py
+++
@@ -1,4 +0,0 @@
-from uteis import numeros
-
-num = input(int('numero: '))
-print(f'o dobro de {num} é {numeros.dobro(num)}')
